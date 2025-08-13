0b298b87d4ef4cbba7d65db2caa070dd49db7711
rabitis99 <kyungsu9914@gmail.com>
2025-08-13 16:00:29 +0900

    fix redis cluster script

--- src/main/java/com/example/emergencyassistb4b4/global/config/redis/RedisConfig.java
+++ src/main/java/com/example/emergencyassistb4b4/global/config/redis/RedisConfig.java
@@ -3,12 +3,31 @@ package com.example.emergencyassistb4b4.global.config.redis;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.data.redis.cluster.nodes}")
+    private String[] clusterNodes;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        List<String> nodes = Arrays.stream(clusterNodes)
+                .map(String::trim)
+                .collect(Collectors.toList());
+
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(nodes);
+        return new LettuceConnectionFactory(redisClusterConfiguration);
+    }
 
     @Bean
     public RedisTemplate<String, Object> objectRedisTemplate(RedisConnectionFactory connectionFactory) {
